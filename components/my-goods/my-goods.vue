f95a9561a4f2afad07f7c17dd9ac5082a47b2b99
buminglang <1574922854@qq.com>
2023-03-06 19:53:32 +0800

    完成了列表

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class="goods-item">
+		<!-- zuocede1左左左 -->
+		<view class="goods-item-left">
+			<image :src="goods.goods_small_logo || defaultPic" mode="" class="goods_pic"></image>
+		</view>
+		<!-- 右右右 -->
+		<view class="goods-item-right">
+			<!-- 商品的名字 -->
+			<view class="goods-name">
+				{{goods.goods_name}}
+			</view>
+			<!-- 商品的价格 -->
+			<view class="goods-info-box">
+				<view class="goods-price">
+					￥{{goods.goods_price | tofixeds}}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		props:{
+			goods:{
+				type: Object,
+				default:{}
+			}
+		},
+		data() {
+			
+			return {
+				//默认图片
+				defaultPic:'https://i2.hdslb.com/bfs/archive/120623ef29707951857ff8e274e67a672ce71cad.jpg@672w_378h_1c_!web-home-common-cover.avif'
+			};
+		},
+		filters: {
+			tofixeds(num) {
+				return Number(num).toFixed(2)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.goods-item{
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #fofofo;
+		.goods-item-left{
+			margin-right: 5px;
+			.goods_pic{
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+		.goods-item-right{
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			.goods-name{
+				font-size: 13px;
+			}
+			.goods-info-box{
+				.goods-price{
+					color: #C00000;
+					font-size: 16px;
+				}
+			}
+		}
+	}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		
+		<view class="goods-list">
+			<view v-for="(goods,i) in goodsList" :key="i" @click="gotoDetail(goods)">
+				<my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +12,67 @@
 	export default {
 		data() {
 			return {
-				
+				//请求参数对象
+				queryObj:{
+					query:'',
+					cid:'',
+					pagenum:1,
+					pagesize:20,
+					
+				},
+				goodsList:[],
+				total: 0,
+				isloading: false
 			};
+		},
+		onLoad(options) {
+			// console.log(options)
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			this.getGoodsList()
+		},
+		methods:{
+			async getGoodsList(cd){
+				this.isloading = true
+				const {'data':res} = await uni.$http.get('https://api-hmugo-web.itheima.net/api/public/v1/goods/search',this.queryObj)
+				this.isloading = false
+				cd && cd()
+				if(res.meta.status !== 200){
+				 	return uni.$showMsg()
+				 }
+				 
+				this.goodsList = [...this.goodsList,...res.message.goods]
+				this.total = res.message.total
+				
+				 
+				 
+			},
+			gotoDetail(goods){
+				uni.navigateTo({
+					url:'/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+				})
+			}
+		},
+		onReachBottom(){
+			if(this.isloading) return
+			this.queryObj.pagenum++
+			this.getGoodsList()
+		},
+		onPullDownRefresh(){
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList= []
+			
+			this.getGoodsList(()=>{
+				//关闭下拉刷新
+				uni.stopPullDownRefresh()
+			})
 		}
+		
 	}
 </script>
 
 <style lang="scss">
-
+	
 </style>
